d8f4a4261173e446f014c181954f26e3075aee9a
RIFAT ABRAR JOWAD <rajdiprifat2004@gmail.com>
2021-07-15 20:31:39 +0600

    const

---
+++ hasinVai/01/04.php
@@ -0,0 +1,24 @@
+<?php $name = "Rifat";
+
+//string
+var_dump($name); //string(5) "Rifat"
+
+
+//int
+$age = 17;
+var_dump($age); //int(17)
+
+//float
+$year = 1.6;
+var_dump($year);//float(1.6)
+
+//bool
+$or = true;
+var_dump($or); //bool(true)
+// NULL
+$ntg = null;
+var_dump($ntg);//NULL
+
+
+
+//comment
